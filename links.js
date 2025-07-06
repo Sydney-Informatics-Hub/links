70898429a8fd0f31af15ce98929e8ef4be61195b
Gordon McDonald <gordon27@gmail.com>
2025-07-07 09:51:48 +1000

    Update links.js

--- links.js
+++ links.js
@@ -78,9 +78,45 @@ const REDIRECTS = {
   },
   'upcoming': {
     url: 'https://unisyd.sharepoint.com/:l:/r/teams/SydneyInformaticsHub2/Lists/Upcoming Informatics Team Events',
-    description: 'Upcoming SIH events'
+    description: 'Upcoming Informatics Team Events'
+  },
+  'leave': {
+    url:'https://unisyd.sharepoint.com/:x:/r/teams/SydneyInformaticsHub2/Shared Documents/1 SIH Central Document Repository/Admin/Leave and Travel Schedule/SIH_Annual_Leave_and_Business_Travel_Schedule_2025.xlsx',
+    description:'SIH Leave Calendar 2025'
+  },
+  'team-guides': {
+    url:'https://unisyd.sharepoint.com/:f:/r/teams/SydneyInformaticsHub2/Shared Documents/1 SIH Central Document Repository/Team Folders/Informatics/Team Guides',
+    description:'SIH Informatics Team Guides'
+  },
+  'vm-register': {
+    url:'https://unisyd.sharepoint.com/:x:/r/teams/SydneyInformaticsHub2/Shared Documents/1 SIH Central Document Repository/Team Folders/Informatics/VM and asset management/VM register.xlsx?d=w4acfbde7177a4af9931fb4b7697bbe16&csf=1&web=1&e=huIcPk&nav=MTJfQzEzX3tDMzY3RTQxNy1CQTFCLTNCNDQtQTYyQy04MzA4MzMxN0ZBNkR9',
+    description:'SIH team VM register'
+  },
+  'client-dashboard': {
+    url:'https://app.powerbi.com/groups/6d62871b-a2c5-4c79-bb85-75eb03e90a2d/reports/700a9b42-6a01-44ce-ac7f-a7e0eeeffc35?ctid=82b3e37e-8171-485d-b10b-38dae7ed14a8',
+    description:'SIH Client Dashboard'
+     },
+  'training-dashboard': {
+    url:'https://app.powerbi.com/groups/6d62871b-a2c5-4c79-bb85-75eb03e90a2d/reports/19d2fbe9-de61-4ea1-bac0-773db6a07aa2?ctid=82b3e37e-8171-485d-b10b-38dae7ed14a8&pbi_source=linkShare',
+    description:'SIH Training Dashboard'
+  },
+  'skill': {
+    url:'https://github.sydney.edu.au/informatics/trainingmaterials/tree/master/CrossSkilling',
+    description:'SIH Cross-skilling calendar + materials'
+  },
+  'monday-update': {
+    url:'https://ctdshub.atlassian.net/wiki/spaces/INFRA/pages/602537987/Monday+Meeting+Project+Update+Presentation+Roster',
+    description:'SIH Monday fortnightly meeting 5-min Project Update Calendar'
   }
 };
+
+// Template:
+//,
+//  'shortlink': {
+//    url:'',
+//    description:''
+//     }
+//
 
 
 // Catch-all URL for unmatched paths
